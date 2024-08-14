a790ec0859b09d7ef64c0cb265c2a55ec33171c6
Aleksei Zaitsev <drshnitsel@gmail.com>
2024-08-14 19:18:31 +0400

    Initial commit

---
+++ lib/ai_test_writer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ai_test_writer/version"
+
+module AiTestWriter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ai_test_writer/ai_api.rb
@@ -0,0 +1,4 @@
+module AiTestWriter
+  class AiApi
+  end
+end
